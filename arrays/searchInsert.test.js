6fe9cafcd466788c8c92cdd8c363d9b6e666fe73
mcam1010 <malik@sonosim.com>
2020-04-23 15:05:52 -0700

    Adding more tests

--- arrays/searchInsert.test.js
+++ arrays/searchInsert.test.js
@@ -4,3 +4,19 @@ const searchInsert = require('./searchInsert')
 test('searching for a potential target in our given array ', () =>{
 	expect(searchInsert([0,1,2,4,5], 3)).toBe(3);
 }); 
+
+test('searching for a potential target in our given array ', () =>{
+	expect(searchInsert([0], 2)).toBe(1);
+}); 
+
+test('searching for a potential target in our given array ', () =>{
+	expect(searchInsert([0,1,2,3,5,6], 4)).toBe(4);
+}); 
+
+test('searching for a potential target in our given array ', () =>{
+	expect(searchInsert([0,1,2,3,4,5,6], -1)).toBe(0);
+}); 
+
+
+
+
